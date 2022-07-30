b2ecff3c4fde7976ff1ce3b1fe31a3c718487eb5
araggohnxd <matheusnagato300@gmail.com>
2022-07-29 23:04:30 -0300

    Add basic testing script

---
+++ tests/tester.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+
+RED="\e[1;31m"
+GREEN="\e[1;32m"
+PURPLE="\e[1;35m"
+CYAN="\e[1;36m"
+RESET="\e[0m"
+
+TEST="../monitoring |& grep monitoring"
+
+PASSED=0
+TOTAL=0
+COUNT=0
+
+check_valid () {
+	COUNT=$((COUNT + 1))
+	printf "${CYAN}($COUNT)${RESET} ${PURPLE}$1${RESET}"
+	if [[ ! -z $2 ]]; then
+		printf "${RED}FAILED\n${RESET}"
+	else
+		printf "${GREEN}PASSED\n${RESET}"
+		PASSED=$((PASSED + 1))
+	fi
+	printf "${PURPLE}Output:${RESET} $2\n\n"
+	TOTAL=$((TOTAL + 1))
+}
+
+check_invalid () {
+	COUNT=$((COUNT + 1))
+	printf "${CYAN}($COUNT)${RESET} ${PURPLE}$1${RESET}"
+	if [[ -z $2 ]]; then
+		printf "${RED}FAILED\n${RESET}"
+	else
+		printf "${GREEN}PASSED\n${RESET}"
+		PASSED=$((PASSED + 1))
+	fi
+	printf "${PURPLE}Output:${RESET} $2\n\n"
+	TOTAL=$((TOTAL + 1))
+}
+
+make -C ../
+printf "${CYAN}\n-----------{ Starting tests }------------\n${RESET}"
+
+##############################
+#####     VALID FILE     #####
+##############################
+printf "${CYAN}\n=== Valid configurations:\n${RESET}"
+cp valid_configs/valid-config monitoring.db
+check_valid "Testing valid config file... " "$(eval "$TEST")"
+
+###################################
+#####     INVALID COLUMNS     #####
+###################################
+printf "${CYAN}\n=== Invalid columns:\n${RESET}"
+COUNT=0
+cp invalid_configs/bad-http-many-columns monitoring.db # (1)
+check_invalid "Testing too many HTTP columns... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-http-few-columns monitoring.db # (2)
+check_invalid "Testing too few HTTP columns... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-ping-many-columns monitoring.db # (3)
+check_invalid "Testing too many PING columns... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-ping-few-columns monitoring.db # (4)
+check_invalid "Testing too few PING columns... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-dns-many-columns monitoring.db # (5)
+check_invalid "Testing too many DNS columns... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-dns-few-columns monitoring.db # (6)
+check_invalid "Testing too few DNS columns... " "$(eval "$TEST")"
+
+####################################
+#####     INVALID PROTOCOL     #####
+####################################
+printf "${CYAN}\n=== Invalid protocols:\n${RESET}"
+COUNT=0
+cp invalid_configs/bad-protocol-1 monitoring.db # (1)
+check_invalid "Testing invalid protocol... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-protocol-2 monitoring.db # (2)
+check_invalid "Testing invalid protocol... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-protocol-3 monitoring.db # (3)
+check_invalid "Testing invalid protocol... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-protocol-4 monitoring.db # (4)
+check_invalid "Testing invalid protocol... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-protocol-5 monitoring.db # (5)
+check_invalid "Testing invalid protocol... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-protocol-6 monitoring.db # (6)
+check_invalid "Testing invalid protocol... " "$(eval "$TEST")"
+
+####################################
+#####     INVALID INTERVAL     #####
+####################################
+printf "${CYAN}\n=== Invalid intervals:\n${RESET}"
+COUNT=0
+cp invalid_configs/bad-interval-1 monitoring.db # (1)
+check_invalid "Testing invalid interval... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-interval-2 monitoring.db # (2)
+check_invalid "Testing invalid interval... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-interval-3 monitoring.db # (3)
+check_invalid "Testing invalid interval... " "$(eval "$TEST")"
+
+##################################
+#####     INVALID METHOD     #####
+##################################
+printf "${CYAN}\n=== Invalid HTTP methods:\n${RESET}"
+COUNT=0
+cp invalid_configs/bad-method-1 monitoring.db # (1)
+check_invalid "Testing invalid method... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-method-2 monitoring.db # (2)
+check_invalid "Testing invalid method... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-method-3 monitoring.db # (3)
+check_invalid "Testing invalid method... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-method-4 monitoring.db # (4)
+check_invalid "Testing invalid method... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-method-5 monitoring.db # (5)
+check_invalid "Testing invalid method... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-method-6 monitoring.db # (6)
+check_invalid "Testing invalid method... " "$(eval "$TEST")"
+
+################################
+#####     INVALID CODE     #####
+################################
+printf "${CYAN}\n=== Invalid HTTP codes:\n${RESET}"
+COUNT=0
+cp invalid_configs/bad-code-1 monitoring.db # (1)
+check_invalid "Testing invalid code... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-code-2 monitoring.db # (2)
+check_invalid "Testing invalid code... " "$(eval "$TEST")"
+
+cp invalid_configs/bad-code-3 monitoring.db # (3)
+check_invalid "Testing invalid code... " "$(eval "$TEST")"
+
+cp valid_configs/valid-config monitoring.db # restore original monitoring.db
+
+printf "${CYAN}\n---------{ Passed %d out of %d }---------\n\n${RESET}" $PASSED $TOTAL
+
+if [ $PASSED -ne $TOTAL ]; then
+	exit 1
+fi
