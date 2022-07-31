94ff1ea843225ffe868d9025ecfd99043b5125b4
araggohnxd <matheusnagato300@gmail.com>
2022-07-31 20:46:17 -0300

    Treat code and interval as integers, not strings

--- sources/protocol_node.c
+++ sources/protocol_node.c
@@ -7,9 +7,7 @@ void	free_protocol_node(void *node)
 	aux = (t_protocol *) node;
 	ft_memfree((void *) &aux->name);
 	ft_memfree((void *) &aux->address);
-	ft_memfree((void *) &aux->interval);
 	ft_memfree((void *) &aux->method);
-	ft_memfree((void *) &aux->code);
 	ft_memfree((void *) &aux->dns_server);
 	ft_memfree((void *) &aux);
 }
@@ -27,12 +25,12 @@ t_protocol	*create_protocol_node(t_protocol_type protocol, char **split)
 	if (protocol == HTTP)
 	{
 		node->method = strdup(split[HTTP_INDEX_METHOD]);
-		node->code = strdup(split[HTTP_INDEX_CODE]);
-		node->interval = strdup(split[HTTP_INDEX_INTERVAL]);
+		node->interval = atoi(split[HTTP_INDEX_INTERVAL]);
+		node->code = atoi(split[HTTP_INDEX_CODE]);
 	}
 	if (protocol == DNS)
 		node->dns_server = strdup(split[DNS_INDEX_SERVER]);
 	if (protocol == PING || protocol == DNS)
-		node->interval = strdup(split[INDEX_INTERVAL]);
+		node->interval = atoi(split[INDEX_INTERVAL]);
 	return (node);
 }
